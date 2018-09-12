6133575ce49c02ba602f6e21577f2de82adf4d5b
Maria Yala <denise_myala@yahoo.com>
2018-09-12 00:11:28 -0400

    Created the landing page with button links to login and signup

---
+++ public_html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SOS App</title>
+    </head>
+    <body>
+        <h1>SOS:An emotion AI</h1>
+        <div id="buttons">
+            <ul>
+                <li>Login</li>
+                <li>Signup</li>
+            </ul>
+        </div>
+    </body>
+</html>

194320357aa733673c0fce538a41b1122dc583dd
Maria Yala <denise_myala@yahoo.com>
2018-09-17 06:01:57 -0400

    Added a dashboard for the warriors

--- public_html/wdashboard.php
+++ public_html/wdashboard.php
@@ -5,7 +5,8 @@
         <link type="text/css" rel="stylesheet" href="./css/warriordash.css">
     </head>
     <body>
-        <h3>Set availability</h3>
+        <h1>Welcome Warrior!</h1>
+        <h3>Set your availability</h3>
         <label class="switch">
             <input type="checkbox" checked>
             <div class="slider round">
@@ -13,5 +14,23 @@
                 <span class="unavailable">UNAVAILABLE</span>
             </div>
         </label>
+        <br>
+        <div>
+            <br>
+            <span class="cta">You have helped save<h3>63</h3>people</span>
+        </div>
+        <div>
+            <h4>NO CALLS FOR HELP AT THE MOMENT</h4>
+            <button>Send a hug</button>
+            <button>Call for help</button>
+            <button>Initiate virtual hangout</button>
+        <div>
+        <div>
+            <br>
+            <span class="cta">If you would like to quit your role as a warrior,<br>
+                Click the button below</span>
+                <br><br>
+            <a href="http://explorations.dev" class="accessbtns">QUIT</a>
+        </div>
     </body>
 </html>
